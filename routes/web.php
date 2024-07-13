b15a92a069ae4e922b6538e0eafdc18abedcd880
FlyingTrowel <aiemanhakim2@gmail.com>
2024-07-13 23:36:36 +0800

    Done Final

--- routes/web.php
+++ routes/web.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Dashboard', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->middleware(['auth', 'verified'])->name('dashboard');;
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
